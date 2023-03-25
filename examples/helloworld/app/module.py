ae12fc0fd6c92aea6b3ad237623de5bb57660e7e
Giovanne Feitosa Afonso <giovanneafonso@gmail.com>
2023-03-25 13:49:22 -0300

    dependency injection docs

--- examples/helloworld/app/module.py
+++ examples/helloworld/app/module.py
@@ -1,13 +1,13 @@
 from bottlenest.common import Module
-from ..users.module import UsersModule
+# from ..users.module import UsersModule
 from ..app.controller import AppController
 from ..app.service import AppService
 
 
 @Module(
-    imports=[UsersModule],
-    controllers=[AppController],
+    # imports=[UsersModule],
     providers=[AppService],
+    controllers=[AppController],
 )
 class AppModule:
     pass
